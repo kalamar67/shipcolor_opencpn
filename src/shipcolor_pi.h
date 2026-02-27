c37a1771cd29744f15477919595eeb308e422076
kalamar67 <inanc.luleci@gmail.com>
2026-02-27 10:23:20 +0300

    Refactor shipcolor_pi.h for improved readability

--- src/shipcolor_pi.h
+++ src/shipcolor_pi.h
@@ -5,65 +5,46 @@
 #include "wx/wx.h"
 #endif
 
-#include "ocpn_plugin.h"   // OpenCPN Plugin API
+#include "ocpn_plugin.h"
 
 #define SHIPCOLOR_PI_VERSION_MAJOR 1
 #define SHIPCOLOR_PI_VERSION_MINOR 0
+#define MY_API_VERSION_MAJOR 1
+#define MY_API_VERSION_MINOR 16
 
-// -------------------------------------------------------
-// AIS Ship Type kodları (ITU-R M.1371-5)
-// -------------------------------------------------------
 enum ShipCategory {
-    SC_UNKNOWN     = 0,
-    SC_FISHING     = 1,   // 30
-    SC_TOWING      = 2,   // 21-24
-    SC_SPEEDBOAT   = 3,   // 31-32
-    SC_PASSENGER   = 4,   // 60-69
-    SC_CARGO       = 5,   // 70-79
-    SC_TANKER      = 6,   // 80-89
-    SC_MILITARY    = 7,   // 35
-    SC_OTHER       = 8
+    SC_UNKNOWN=0, SC_FISHING, SC_TOWING, SC_SPEEDBOAT,
+    SC_PASSENGER, SC_CARGO, SC_TANKER, SC_MILITARY, SC_OTHER
 };
 
-// -------------------------------------------------------
-// Renk yapısı
-// -------------------------------------------------------
-struct ShipColor {
-    unsigned char r, g, b;
-};
+struct ShipColor { unsigned char r, g, b; };
 
-// -------------------------------------------------------
-// Plugin sınıfı
-// -------------------------------------------------------
 class shipcolor_pi : public opencpn_plugin_18 {
 public:
     shipcolor_pi(void* ppimgr);
     ~shipcolor_pi();
 
-    // --- Zorunlu OpenCPN API metodları ---
-    int         Init() override;
-    bool        DeInit() override;
-    int         GetAPIVersionMajor() override;
-    int         GetAPIVersionMinor() override;
-    int         GetPlugInVersionMajor() override;
-    int         GetPlugInVersionMinor() override;
-    wxString    GetCommonName() override;
-    wxString    GetShortDescription() override;
-    wxString    GetLongDescription() override;
-    wxBitmap*   GetPlugInBitmap() override;
-
-    // --- Render callback ---
-    bool RenderOverlay(wxDC& dc, PlugIn_ViewPort* vp) override;
-    bool RenderOverlayMultiCanvas(wxDC& dc, PlugIn_ViewPort* vp, int canvas_index) override;
+    int       Init() override;
+    bool      DeInit() override;
+    int       GetAPIVersionMajor() override;
+    int       GetAPIVersionMinor() override;
+    int       GetPlugInVersionMajor() override;
+    int       GetPlugInVersionMinor() override;
+    wxString  GetCommonName() override;
+    wxString  GetShortDescription() override;
+    wxString  GetLongDescription() override;
+    wxBitmap* GetPlugInBitmap() override;
+    bool      RenderOverlay(wxDC& dc, PlugIn_ViewPort* vp) override;
+    void      SetDefaults() override {}
+    int       GetToolbarToolCount() override { return 0; }
+    void      OnToolbarToolCallback(int id) override {}
 
 private:
-    ShipCategory  GetCategory(int ship_type);
-    ShipColor     GetColor(ShipCategory cat);
-    void          DrawFilledTriangle(wxDC& dc, 
-                                     wxPoint center, 
-                                     double heading_deg,
-                                     double scale,
-                                     ShipColor color);
-
+    ShipCategory GetCategory(int ship_type);
+    ShipColor    GetColor(ShipCategory cat);
+    void         DrawFilledTriangle(wxDC& dc, wxPoint center,
+                                    double heading_deg, double scale,
+                                    ShipColor color);
     wxBitmap m_logo;
 };
+   
